f6cd8392018cddce0f108c00370cf022217446dc
Ingridoliveira <oligrid20@gmail.com>
2025-01-23 00:12:38 -0300

    stely:Estrutura da aplicação - Styled Components

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import styled, { css } from "styled-components";
+
+export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
+
+interface ButtonContainerProps {
+  variant: ButtonVariant;
+}
+
+const buttonVariantes = {
+  primary: "purple",
+  secondary: "orange",
+  danger: "red",
+  success: "green",
+};
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+
+  background-color: ${props => props.theme["green-500"]};
+`
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer } from "./styles";
+
+export const Header = () => {
+  return (
+    <HeaderContainer>
+      <span>logo</span>
+      <nav>
+        <a href="http://">time</a>
+        <a href="http://">hystory</a>
+      </nav>
+    </HeaderContainer>
+  );
+};
