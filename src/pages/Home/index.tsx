27e093f7d46d438c48160c5b241fad47bcb883fb
Ingridoliveira <oligrid20@gmail.com>
2025-01-24 23:41:06 -0300

    Commit: Páginas e rotas - Aprimorando os inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import {  Play } from "phosphor-react";
+import { Play } from "phosphor-react";
 import {
   CountdownContainer,
   FormeContainer,
@@ -15,10 +15,27 @@ export const Home = () => {
       <form action="">
         <FormeContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput type="task" placeholder="Dê um nome para seu projeto"/>
+          <TaskInput
+            type="task"
+            list="task-suggrstions"
+            placeholder="Dê um nome para seu projeto"
+          />
+
+          <datalist id="task-suggrstions">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+            <option value="Projeto 3"></option>
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput type="number" id="minutesAmount"  placeholder="00"/>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
           <samp>minutos.</samp>
         </FormeContainer>
 
@@ -33,7 +50,7 @@ export const Home = () => {
           <Play size={24} />
           Começar
         </StartCountdownButton>
-      </form> 
+      </form>
     </HomeContainer>
   );
 };
